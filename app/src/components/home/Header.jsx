b24ad60501ec86a1e61d739ef87db24d47340ffd
Luthfi Aji W <luthfi.ajiw@gmail.com>
2019-09-22 13:20:39 +0700

    add ink

--- app/src/components/home/Header.jsx
+++ app/src/components/home/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import styles from './styles/header.css';
-import bestLogo from '../../../assets/img/best-logo-512.png';
+import bestBrand from '../../../assets/img/best-brand.png';
 
 const Header = () => (
   <div className="container-fluid">
     <nav className="navbar navbar-expand-lg shadow-none d-flex justify-content-between">
       <span className="navbar-brand d-flex align-items-end">
-        <img src={bestLogo} className="d-inline-block" width="60" height="50" alt="logo" />
-        <span className={styles.bestLogo}>Best</span>
+        <img
+          src={bestBrand}
+          className="d-inline-block"
+          width="145"
+          height="50"
+          alt="logo"
+        />
       </span>
       <i className={`far fa-power-off text-secondary ${styles.powerbutton}`} />
     </nav>
--- app/src/components/home/Menu.jsx
+++ app/src/components/home/Menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import Ink from 'react-ink';
 
 import styles from './styles/menu.css';
 
@@ -10,12 +11,14 @@ const Menu = () => (
         <div className={styles.menu}>
           <i className="far fa-folder-plus fa-4x" />
           <p>Membuat Data Baru</p>
+          <Ink />
         </div>
       </div>
       <div className={styles.card_menu}>
         <div className={styles.menu}>
           <i className="far fa-folder-open fa-4x" />
           <p>Buka Data</p>
+          <Ink />
         </div>
       </div>
     </Row>
@@ -24,12 +27,14 @@ const Menu = () => (
         <div className={styles.menu}>
           <i className="far fa-folders fa-4x" />
           <p>Contoh Data</p>
+          <Ink />
         </div>
       </div>
       <div className={styles.card_menu}>
         <div className={styles.menu}>
           <i className="far fa-tasks fa-4x" />
           <p>Daftar Data</p>
+          <Ink />
         </div>
       </div>
     </Row>
